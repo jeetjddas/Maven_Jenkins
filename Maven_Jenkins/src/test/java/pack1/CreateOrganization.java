e819de9bceca455fb573eeb90a79e0f34731ff96
JEET JD DAS <JEET JD DAS@DESKTOP-K57E8BB>
2020-08-08 01:29:08 +0530

    1.2 commit

--- Maven_Jenkins/src/test/java/pack1/CreateOrganization.java
+++ Maven_Jenkins/src/test/java/pack1/CreateOrganization.java
@@ -1,8 +1,13 @@
 package pack1;
 
+import org.testng.annotations.Test;
+
 public class CreateOrganization {
 	
 	@Test
 	public void createOrganization() {}
+	
+	@Test
+	public void deleteOrganization() {}
 
 }
